76ac76f0585ada57e3a4be1cb502940e6f86d735
Alex <aeversme@gmail.com>
2021-07-23 09:42:00 -0400

    refactor transformation code

--- etl.py
+++ etl.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from urllib.request import urlretrieve
+from transform import date_series_to_datetime, filter_dataframe, merge_dataframes
 
 NYT_DATA = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'
 HOPKINS_DATA = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
@@ -10,21 +11,16 @@ urlretrieve(HOPKINS_DATA, 'hopkins_data.csv')
 
 # Read data from each .csv into a Pandas dataframe
 nyt_df = pd.read_csv('nyt_data.csv')
-hopkins_data = pd.read_csv('hopkins_data.csv')
+hopkins_df = pd.read_csv('hopkins_data.csv')
 
 # Transform Johns Hopkins data
-hopkins_data.rename(columns=str.lower, inplace=True)
-hopkins_data['date'] = pd.to_datetime(hopkins_data['date'])
-hopkins_data_filtered = hopkins_data[hopkins_data['country/region'] == 'US']
-hopkins_data_reduced = hopkins_data_filtered.drop(columns=['country/region',
-                                                           'province/state',
-                                                           'confirmed',
-                                                           'deaths'])
+hopkins_transformed = filter_dataframe(hopkins_df)
 
 #  Transform NYT dates to datetime objects
-nyt_df['date'] = pd.to_datetime(nyt_df['date'])
+nyt_df['date'] = date_series_to_datetime(nyt_df)
 
 # Merge the dataframes, dropping rows that don't share the same datetime
-covid_data = nyt_df.merge(hopkins_data_reduced, on='date', how='inner')
+covid_data = merge_dataframes(nyt_df, hopkins_transformed)
 
 print(covid_data)
+print(covid_data.dtypes)
--- transform.py
+++ transform.py
@@ -6,12 +6,12 @@ def date_series_to_datetime(dataframe):
 
 
 def filter_dataframe(dataframe):
-    dataframe_lowercase_titles = dataframe.rename(columns=str.lower, inplace=True)
-    dataframe_datetime = date_series_to_datetime(dataframe_lowercase_titles)
-    dataframe_filtered = dataframe_datetime[dataframe_datetime['country/region'] == 'US']
+    dataframe.rename(columns=str.lower, inplace=True)
+    dataframe['date'] = date_series_to_datetime(dataframe)
+    dataframe_filtered = dataframe[dataframe['country/region'] == 'US']
     dataframe_reduced = dataframe_filtered.drop(columns=['country/region', 'province/state', 'confirmed', 'deaths'])
     return dataframe_reduced
 
 
 def merge_dataframes(dataframe_1, dataframe_2):
-    return dataframe_1.merge(dataframe_2, on='date', how='inside')
+    return dataframe_1.merge(dataframe_2, on='date', how='inner')
