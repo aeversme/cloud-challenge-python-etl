84b9a0991ec29a40a5c62d211e2cb409ddcc4db9
Alex <aeversme@gmail.com>
2021-07-24 00:43:08 -0400

    refactor etl.py

--- etl.py
+++ etl.py
@@ -16,23 +16,29 @@ def extract_transform_covid_data(nyt_url, hopkins_url):
         # SNS notification
         print("There was an error retrieving a data set.")
         raise
-    else:
-        # Read data from each .csv into a Pandas dataframe
-        nyt_df = pd.read_csv('nyt_data.csv')
-        hopkins_df = pd.read_csv('hopkins_data.csv')
-
-        # Transform Johns Hopkins data
-        hopkins_transformed = filter_dataframe(hopkins_df)
-        print(hopkins_transformed.columns)
-
-        #  Transform NYT dates to datetime objects
-        nyt_df['date'] = date_series_to_datetime(nyt_df)
-
-        # Merge the dataframes, dropping rows that don't share the same datetime
-        covid_data = merge_dataframes(nyt_df, hopkins_transformed)
-
-        print(covid_data)
-        print(covid_data.dtypes)
+    # Read data from each .csv into a Pandas dataframe
+    nyt_df = pd.read_csv('nyt_data.csv')
+    hopkins_df = pd.read_csv('hopkins_data.csv')
+    return nyt_df, hopkins_df
 
 
-extract_transform_covid_data(NYT_DATA, HOPKINS_DATA)
+def transform_dataframes(nyt_df, hopkins_df):
+    # Transform Johns Hopkins data
+    hopkins_transformed = filter_dataframe(hopkins_df)
+    #  Transform NYT dates to datetime objects
+    nyt_df['date'] = date_series_to_datetime(nyt_df)
+    return nyt_df, hopkins_transformed
+
+
+def covid_data_merge(nyt_df, hopkins_df):
+    # Merge the dataframes, dropping rows that don't share the same datetime
+    merged_covid_data = merge_dataframes(nyt_df, hopkins_df)
+    return merged_covid_data
+
+
+nyt_df, hopkins_df = extract_transform_covid_data(NYT_DATA, HOPKINS_DATA)
+transformed_nyt_df, transformed_hopkins_df = transform_dataframes(nyt_df, hopkins_df)
+covid_data = covid_data_merge(transformed_nyt_df, transformed_hopkins_df)
+
+print(covid_data)
+print(covid_data.dtypes)
