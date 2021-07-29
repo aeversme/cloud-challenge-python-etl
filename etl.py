17e3d973a0d3363c6ca27de1c5464c6f4398466c
Alex <aeversme@gmail.com>
2021-07-28 23:48:49 -0400

    refactor pipeline to handle exceptions

--- etl.py
+++ etl.py
@@ -41,50 +41,70 @@ def merge_covid_dataframes(nyt_df, hopkins_df):
     return merged_covid_data
 
 
-try:
-    nyt_df, hopkins_df = download_covid_dataframe(NYT_DATASET_URL, HOPKINS_DATASET_URL)
-except urllib.error.HTTPError:
-    print("The import failed")
-    # TODO SNS error notification
-try:
-    transformed_nyt_df, transformed_hopkins_df = transform_dataframes(nyt_df, hopkins_df)
-except ValueError:
-    print("Some data was invalid")
-    # TODO SNS error notification
-covid_data = merge_covid_dataframes(transformed_nyt_df, transformed_hopkins_df)
+def extract_transform():
+    try:
+        nyt_df, hopkins_df = download_covid_dataframe(NYT_DATASET_URL, HOPKINS_DATASET_URL)
+    except urllib.error.HTTPError:
+        print("The import failed")
+        # TODO SNS error notification
+        raise
+    try:
+        transformed_nyt_df, transformed_hopkins_df = transform_dataframes(nyt_df, hopkins_df)
+    except ValueError:
+        print("Some data was invalid")
+        # TODO SNS error notification
+        raise
+    try:
+        covid_data = merge_covid_dataframes(transformed_nyt_df, transformed_hopkins_df)
+    except:
+        print("The merge failed")
+        # TODO SNS error notification
+        raise
+    return covid_data
 
-# print(covid_data)
 
-data_container = CovidDataContainer()
+def load_to_database():
+    try:
+        covid_data = extract_transform()
 
-for index, row in covid_data.iterrows():
-    data_container.add_day(CovidDayStats(str(row.date), row.cases, row.deaths, row.recovered))
-    # if index >= 548:
-    #     break
-# print(data_container.get_day_with_timestamp(pd.Timestamp("2020-12-01")))
+        data_container = CovidDataContainer()
 
-most_recent_dataset_date = covid_data['date'][covid_data.index[-1]]
-most_recent_database_date = data_container.get_most_recent_date()
+        # for index, row in covid_data.iterrows():
+        #     data_container.add_day(CovidDayStats(str(row.date), row.cases, row.deaths, row.recovered))
+        #     if index >= 548:
+        #         break
 
-if len(data_container) == 0:
-    print("Loading initial data into database...")
-    for index, row in covid_data.iterrows():
-        data_container.add_day(CovidDayStats(str(row.date), row.cases, row.deaths, row.recovered))
-    print(len(data_container))
-elif most_recent_dataset_date > most_recent_database_date:
-    print("Loading only new data into database...")
-    # find dataset index of most recent database date
-    dataset_index_of_database_most_recent_date = int(covid_data[covid_data['date'] ==
-                                                                most_recent_database_date].index.values)
-    # eliminate all dataset indices prior to most recent database date index
-    new_covid_data = covid_data.drop(covid_data.index[range(0, dataset_index_of_database_most_recent_date + 1)])
-    # print modified dataframe
-    print(most_recent_database_date)
-    print(new_covid_data)
-    # use data_container.add_day to add only new data to database
-    for index, row in new_covid_data.iterrows():
-        data_container.add_day(CovidDayStats(str(row.date), row.cases, row.deaths, row.recovered))
-    print(data_container.get_most_recent_date())
-    # TODO SNS notification of update completion, include # of rows updated
-else:
-    print("No new data to load")
+        most_recent_dataset_date = covid_data['date'][covid_data.index[-1]]
+        most_recent_database_date = data_container.get_most_recent_date()
+
+        if len(data_container) == 0:
+            print("Loading initial data into database...")
+            for index, row in covid_data.iterrows():
+                data_container.add_day(CovidDayStats(str(row.date), row.cases, row.deaths, row.recovered))
+            print(f"Initial data loaded, added {len(data_container)} rows to the database")
+            # TODO SNS notification of initial load completion, include # of rows updated
+        elif most_recent_dataset_date > most_recent_database_date:
+            print("Loading only new data into database...")
+            # find dataset index of most recent database date
+            dataset_index_of_database_most_recent_date = int(covid_data[covid_data['date'] ==
+                                                                        most_recent_database_date].index.values)
+            # eliminate all dataset indices prior to most recent database date index
+            new_covid_data = covid_data.drop(covid_data.index[range(0, dataset_index_of_database_most_recent_date + 1)])
+            # print modified dataframe
+            print(most_recent_database_date)
+            print(new_covid_data)
+            # use data_container.add_day to add only new data to database
+            for index, row in new_covid_data.iterrows():
+                data_container.add_day(CovidDayStats(str(row.date), row.cases, row.deaths, row.recovered))
+            print(data_container.get_most_recent_date())
+            print(f"Update successful, added {len(new_covid_data)} rows to the database")
+            # TODO SNS notification of update completion, include # of rows updated
+        else:
+            print("No new data to load")
+            # TODO SNS notification of no update
+    except:
+        print("Something went wrong")
+
+
+if __name__ == "__main__":
+    load_to_database()
