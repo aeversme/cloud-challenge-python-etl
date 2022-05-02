489d865cbbb84b7d7283df122e33d09c87e96277
Alex <aeversme@gmail.com>
2022-05-02 15:47:40 -0400

    change AWS provider version

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.27"
+      version = "4.12.1"
     }
     random = {
       source  = "hashicorp/random"
